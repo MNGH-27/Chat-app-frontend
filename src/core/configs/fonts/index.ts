ebd44ac25c59d20c0a4dcbb39a9d513be9726f8c
mohsen noury <66866297+MNGH-27@users.noreply.github.com>
2023-12-12 08:58:05 +0330

    fix: fix folder structure of font

--- src/core/configs/fonts/index.ts
+++ src/core/configs/fonts/index.ts
@@ -1,5 +1,126 @@
-import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 
-const inter = Inter({ subsets: ['latin'] })
+const josefin = localFont({
+    src: [
+        {
+            path: './../../../../public/fonts/Josefin_Sans/static/JosefinSans-Thin.ttf',
+            weight: '100',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Josefin_Sans/static/JosefinSans-ExtraLight.ttf',
+            weight: '200',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Josefin_Sans/static/JosefinSans-Light.ttf',
+            weight: '300',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Josefin_Sans/static/JosefinSans-Regular.ttf',
+            weight: '400',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Josefin_Sans/static/JosefinSans-Medium.ttf',
+            weight: '500',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Josefin_Sans/static/JosefinSans-SemiBold.ttf',
+            weight: '600',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Josefin_Sans/static/JosefinSans-Bold.ttf',
+            weight: '700',
+            style: 'normal'
+        }
+    ],
+    variable: '--font-josefin'
+})
 
-export { inter }
+const montserrat = localFont({
+    src: [
+        {
+            path: './../../../../public/fonts/Montserrat_Alternates/MontserratAlternates-Thin.ttf',
+            weight: '100',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Montserrat_Alternates/MontserratAlternates-ExtraLight.ttf',
+            weight: '200',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Montserrat_Alternates/MontserratAlternates-Light.ttf',
+            weight: '300',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Montserrat_Alternates/MontserratAlternates-Regular.ttf',
+            weight: '400',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Montserrat_Alternates/MontserratAlternates-Medium.ttf',
+            weight: '500',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Montserrat_Alternates/MontserratAlternates-SemiBold.ttf',
+            weight: '600',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Montserrat_Alternates/MontserratAlternates-Bold.ttf',
+            weight: '700',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Montserrat_Alternates/MontserratAlternates-ExtraBold.ttf',
+            weight: '800',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Montserrat_Alternates/MontserratAlternates-Black.ttf',
+            weight: '900',
+            style: 'normal'
+        }
+    ],
+    variable: '--font-montserrat'
+})
+
+const comfortaa = localFont({
+    src: [
+        {
+            path: './../../../../public/fonts/Comfortaa/static/Comfortaa-Light.ttf',
+            weight: '300',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Comfortaa/static/Comfortaa-Regular.ttf',
+            weight: '400',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Comfortaa/static/Comfortaa-Medium.ttf',
+            weight: '500',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Comfortaa/static/Comfortaa-SemiBold.ttf',
+            weight: '600',
+            style: 'normal'
+        },
+        {
+            path: './../../../../public/fonts/Comfortaa/static/Comfortaa-Bold.ttf',
+            weight: '700',
+            style: 'normal'
+        }
+    ],
+    variable: '--font-comfortaa'
+})
+
+export { josefin, montserrat, comfortaa }
