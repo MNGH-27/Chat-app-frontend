5e1fe979fa12bd1e4ee4f59662739b95e831ccf3
mohsen noury <66866297+MNGH-27@users.noreply.github.com>
2023-12-25 09:27:18 +0330

    fix: add color plate to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,43 @@ const config: Config = {
     },
     important: true,
     content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+
     theme: {
-        fontFamily: {
-            josefin: 'var(--font-josefin)',
-            montserrat: 'var(--font-montserrat)',
-            comfortaa: 'var(--font-comfortaa)'
+        extend: {
+            colors: {
+                brand: {
+                    50: '#E8EDF2',
+                    100: '#D1DBE6',
+                    200: '#A0B5CA',
+                    300: '#7291B1',
+                    400: '#4E6E8D',
+                    500: '#34495E',
+                    600: '#2C3E50',
+                    700: '#1F2B38',
+                    800: '#141C24',
+                    900: '#0B0F14',
+                    950: '#05080A'
+                },
+                primary: {
+                    DEFAULT: '#3980FD',
+                    50: '#EBF2FF',
+                    100: '#D7E5FF',
+                    200: '#AECCFE',
+                    300: '#86B2FE',
+                    400: '#629BFD',
+                    500: '#3980FD',
+                    600: '#025CF7',
+                    700: '#0244B6',
+                    800: '#012D79',
+                    900: '#01173D',
+                    950: '#000B1E'
+                }
+            },
+            fontFamily: {
+                josefin: 'var(--font-josefin)',
+                montserrat: 'var(--font-montserrat)',
+                comfortaa: 'var(--font-comfortaa)'
+            }
         }
     },
     plugins: []
